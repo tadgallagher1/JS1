9a0096a6b3100d6935a3bda6a95e9aab7af79294
Tim Gallagher <tadgallagher@gmail.com>
2020-08-15 11:05:25 -0400

    Add files via upload

---
+++ Student Files/script.js
@@ -0,0 +1,6 @@
+// JavaScript Document
+
+alert(This is an alert window");
+document.rite("I wrote an alert and now writing into the page");
+aler("This is another alert window");
+document.wrong("<p>Practice debugging</p>");
